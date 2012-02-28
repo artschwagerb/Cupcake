21bd473166a804b191b5f7201a0d8b61683197cf
Brian Artschwager <waterskiaddict@gmail.com>
2012-02-27 18:50:59 -0600

    Classes/Email added.

--- config.php
+++ config.php
@@ -5,19 +5,30 @@ define("DB_HOST", "localhost");
 define( "DB_USERNAME", "" );
 define( "DB_PASSWORD", "" );
 define( "DB_DATABASE", "cupcake" );
+
+define( "EMAIL_USERNAME", "noreply@cupcakesfor.me");
+define( "EMAIL_ADDRESS", "noreply@cupcakesfor.me");
+define( "EMAIL_PASSWORD", "AL1e4aVQAJl1u4u10");
+define( "EMAIL_NAME", "Cupcake");
+define( "EMAIL_HOST", "smtp.gmail.com");
+
+
 define( "CLASS_PATH", "classes" );
 define( "INCLUDE_PATH", "include" );
 define( "TEMPLATE_PATH", "include/templates" );
+
 define( "HOMEPAGE_NUM_ITEMS", 5 );
 define( "TIME_OFFSET", "0 hours");
 
-require( CLASS_PATH . "/class.user.php" );
-require( CLASS_PATH . "/class.databee.php" );
-require( CLASS_PATH . "/class.episode.php" );
-require( CLASS_PATH . "/class.season.php" );
-require( CLASS_PATH . "/class.show.php" );
-require( CLASS_PATH . "/class.movie.php" );
-require( CLASS_PATH . "/class.statistics.php" );
-require( CLASS_PATH . "/class.comment.php" );
-require( CLASS_PATH . "/class.topic.php" );
+require_once( CLASS_PATH . "/class.user.php" );
+require_once( CLASS_PATH . "/class.databee.php" );
+require_once( CLASS_PATH . "/class.episode.php" );
+require_once( CLASS_PATH . "/class.season.php" );
+require_once( CLASS_PATH . "/class.show.php" );
+require_once( CLASS_PATH . "/class.movie.php" );
+require_once( CLASS_PATH . "/class.statistics.php" );
+require_once( CLASS_PATH . "/class.comment.php" );
+require_once( CLASS_PATH . "/class.topic.php" );
+require_once( CLASS_PATH . "/class.admin.php" );
+require_once( CLASS_PATH . "/class.page.php");
 ?>
